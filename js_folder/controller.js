514ec3d211e91e1401017bc2a18fd17a393ba27e
Muh-Uzair <muhammaduzair1062001@gmail.com>
2024-03-05 18:58:51 +0500

    all working

--- js_folder/controller.js
+++ js_folder/controller.js
@@ -7,49 +7,20 @@ import {header, node_list_div_slide, div_all_slides } from "./config.js"
 
 
 let max_slides = "" ;
-let curr_slide = 0 
+let curr_slide = 0 ;
+const img_array = model.state_object.img_array ;
 
 
 
 
 
-function initial_working()
+function control_initial_working()
 {
-    // making the size smaller
-    // header.style.transform = "scale(0.5)" ;
-
-    // initially setting all the slides side by side using tranformX
-    
-    document.querySelectorAll(".div_slide").forEach( (val, i) => { val.style.transform = `translateX(${100*i}%)`}) ; 
-
-    
+    view_1.initial_working() ;
 }
-function preparing_images()
+function control_preparing_images()
 {
-    // 1 : prepare an empty markup             
-    let mark_up = ""
-
-    
-    // 2 : empty the targeted element
-    div_all_slides.innerHTML = mark_up ;
-
-    
-    // 3 : pre_paring the string
-    model.state_object.img_array.forEach( (val , i) =>
-    {
-        mark_up += ` <div class="div_slide div_slide_${i}" >
-                          <img class="img_slide img_slide_${i}" src="${val}.jpg" alt="error">
-                     </div>`
-    })
-    // console.log(mark_up) ;
-
-    // 4 : push it to DOM 
-    div_all_slides.insertAdjacentHTML("afterbegin" , mark_up) ;
-
-    max_slides = document.querySelectorAll(".div_slide").length ;
-
-    // console.log("")
-    // console.log(div_all_slides.innerHTML)
+    max_slides = view_1.preparing_images(img_array) ;
 }
 function control_left_right_buttons_working_function(exact_btn)
 {
@@ -65,7 +36,11 @@ function control_left_right_buttons_working_function(exact_btn)
     }
 
 }
-
+function control_dot_pressing(dot_number)
+{
+    
+    view_1.dot_wokring_function(dot_number) ;
+}
 
 
 
@@ -76,8 +51,10 @@ function control_left_right_buttons_working_function(exact_btn)
 function init()
 {
 
+    view_1.add_event_listener_function_keyboard__buttons(control_left_right_buttons_working_function)
     view_1.add_event_listener_function_left_right_buttons(control_left_right_buttons_working_function) ;
-    preparing_images() ;
-    initial_working() ;
+    view_1.add_event_listener_function_dots(control_dot_pressing) ;
+    control_preparing_images() ;
+    control_initial_working() ;
 }
 init() ;
--- js_folder/view_folder/view_1.js
+++ js_folder/view_folder/view_1.js
@@ -3,6 +3,8 @@
 import {header,node_list_div_slide} from "../config.js"
 
 
+
+// Event Listeners
 export function add_event_listener_function_left_right_buttons(recieved_control_left_right_buttons_working_function)
 {
 
@@ -28,6 +30,39 @@ export function add_event_listener_function_left_right_buttons(recieved_control_
     
 
 }
+export function add_event_listener_function_keyboard__buttons(recieved_control_left_right_buttons_working_function)
+{
+    document.addEventListener("keydown" , 
+    function(event_info_object)
+    {
+        const pressed_key = event_info_object.key ;
+
+        if(pressed_key === `ArrowRight`)
+        {
+            recieved_control_left_right_buttons_working_function(`btn_forward`) ;
+        }
+        else if(pressed_key === `ArrowLeft`)
+        {
+            recieved_control_left_right_buttons_working_function(`btn_back`) ;
+        }
+
+    })
+}
+export function add_event_listener_function_dots(recieved_control_dot_pressing)
+{
+    document.querySelector(".div_dots").addEventListener("click",
+    function(event_info_object)
+    {
+        
+
+        const dot = event_info_object.target.closest(".button_dot") ;
+        if(!dot) return ;
+
+        const dot_number = `${[...dot.classList[1]].splice(-1)}` ;
+        recieved_control_dot_pressing(dot_number) ;
+
+    })
+}
 
 
 
@@ -37,47 +72,158 @@ export function add_event_listener_function_left_right_buttons(recieved_control_
 
 
 
+
+// Functionalities
 function calculation(new_curr_slide)
 {
     document.querySelectorAll(".div_slide").forEach( (val, i) => {  
 
         // console.log(val)
-        val.style.transform = `translateX(${100 * ( i - new_curr_slide)}%)`
+        // 1 : adjusting the translateX of each img 
+        val.style.transform = `translateX(${100 * ( i - new_curr_slide)}%)` ;
+
+        // 2 : removing active class 
+        val.classList.remove("active") ;   
+        
+        // 3 : 
+        document.querySelector(`.button_dot_${i}`).style.backgroundColor = "rgba(255, 255, 255, 0.3)"  ;           
     })
+
+    const active_slide = "" ;
+
 }
 export function btn_forward_working_function(curr_slide ,max_slides)
 {
 
-    console.log(`btn_forward_clicked`)
+    // console.log(`btn_forward_clicked`)
+    // console.log(curr_slide)
 
-    // 1 : 
+    // 1 : makgin the dot color low of previous dot
+    document.querySelector(`.button_dot_${curr_slide}`).style.backgroundColor = "rgba(255, 255, 255, 0.3)" ;
+
+    // 2 : adjusting variables 
     let new_curr_slide = curr_slide ;
+ 
 
     // 2 : if we are at last slide so return to slide 0
     (new_curr_slide === max_slides-1 ) ? ( new_curr_slide = 0 ) : ( new_curr_slide ++ ) ;
 
+
     // 3 : calculation
-    calculation(new_curr_slide)
+    calculation(new_curr_slide) ;
 
-    // 4 : 
+
+    // 4 : adding active class to the currnt img on display
+    document.querySelector(`.div_slide_${new_curr_slide}`).classList.add("active") ;
+
+
+    // 5 : making background color dark of active dot 
+    document.querySelector(`.button_dot_${new_curr_slide}`).style.backgroundColor = "rgba(255, 255, 255, 0.5)" ;
+
+
+    // 6 : return values  
     return new_curr_slide ;
-
 
 }
 export function btn_back_working_function(curr_slide ,max_slides)
 {
-    console.log(`btn_back_clicked`)
+    // console.log(`btn_back_clicked`)
+
+
+    // 1 : makgin the dot color low of previous dot
+    document.querySelector(`.button_dot_${curr_slide}`).style.backgroundColor = "rgba(255, 255, 255, 0.3)" ;
     
-    // 1 : 
+
+    // 2 : adjusting variables 
     let new_curr_slide = curr_slide ;
 
-    // 2 : if we are at slide 0 and we click back so the curr slide should become the last slide
+
+    // 3 : if we are at slide 0 and we click back so the curr slide should become the last slide
     (new_curr_slide === 0) ? (new_curr_slide = max_slides-1) : (new_curr_slide --) ;
 
-    // 3 : calculation
-    calculation(new_curr_slide)
 
-    // 4 : return 
+    // 4 : calculation
+    calculation(new_curr_slide) ;
+
+
+    // 5 : adding active class to the currnt img on display
+    document.querySelector(`.div_slide_${new_curr_slide}`).classList.add("active") ;
+
+    
+    // 6 : making the background color dark
+    document.querySelector(`.button_dot_${new_curr_slide}`).style.backgroundColor = "rgba(255, 255, 255, 0.5)" ;
+
+
+    // 4 : return values 
     return new_curr_slide ;
     
+}
+export function initial_working()
+{
+
+    // 1 : making the size smaller
+    header.style.transform = "scale(0.9)" ;
+
+
+    // 2 : initially setting all the slides side by side using tranformX
+    document.querySelectorAll(".div_slide").forEach( (val, i) => { val.style.transform = `translateX(${100*i}%)`}) ; 
+
+    // 3 : adding actuve class to initial img
+    document.querySelector(".div_slide_0").classList.add("active") ;
+
+    // 4 : making the background color dark 
+    document.querySelector(`.button_dot_0`).style.backgroundColor = "rgba(255, 255, 255, 0.5)";
+
+
+
+}
+export function preparing_images(img_array)
+{
+    // 1 : prepare an empty markup             
+    let mark_up = "" ;
+    let mark_up_dots = "" ;
+
+    
+    // 2 : empty the targeted element
+    document.querySelector(`.div_all_slides`).innerHTML = mark_up ;
+    document.querySelector(".div_dots").innerHTML = mark_up_dots ;
+
+    
+    // 3 : pre_paring the strings
+    img_array.forEach( (val , i) =>
+    {
+        mark_up += ` <div class="div_slide div_slide_${i}" >
+                          <img class="img_slide img_slide_${i}" src="${val}.jpg" alt="error">
+                     </div>`
+
+        mark_up_dots += `<button class="button_dot button_dot_${i}"></button>`
+    })
+    // console.log(mark_up) ;
+
+
+    // 4 : push it to DOM 
+    document.querySelector(`.div_all_slides`).insertAdjacentHTML("afterbegin" , mark_up) ;
+    document.querySelector(".div_dots").insertAdjacentHTML("afterbegin" , mark_up_dots) ;
+    
+
+    // 5 : returning the max_slide_number
+    return document.querySelectorAll(".div_slide").length ;
+
+
+    // console.log("")
+    // console.log(div_all_slides.innerHTML)
+
+}
+export function dot_wokring_function(dot_number)
+{
+    // document.querySelector(`.button_dot_${i}`).style.backgroundColor = "rgba(255, 255, 255, 0.3)"  ; 
+        
+    calculation(dot_number) ;
+
+    // 4 : adding active class to the currnt img on display
+    document.querySelector(`.div_slide_${dot_number}`).classList.add("active") ;
+
+
+    // 5 : making background color dark of active dot 
+    document.querySelector(`.button_dot_${dot_number}`).style.backgroundColor = "rgba(255, 255, 255, 0.5)" ;
 }
